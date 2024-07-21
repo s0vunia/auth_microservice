bdb1e52de22d569ee444dc4d268befe767f2f71f
sovunia <burindima324@gmail.com>
2024-07-21 13:30:45 +0300

    add model

---
+++ internal/model/user.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"database/sql"
+	"time"
+)
+
+// User represents a user entity with ID, Info, CreatedAt, and UpdatedAt fields.
+type User struct {
+	ID        int64
+	Info      UserInfo
+	CreatedAt time.Time
+	UpdatedAt sql.NullTime
+}
+
+// UserInfo represents a user info entity with Name, Email, and Role fields.
+type UserInfo struct {
+	Name  string
+	Email string
+	Role  Role
+}
+
+// UserCreate represents a user create entity with Name, Email, Role, and Password fields.
+type UserCreate struct {
+	Name     string
+	Email    string
+	Role     Role
+	Password string
+}
+
+// UserUpdate represents a user update entity with Name, Email, and Role fields.
+type UserUpdate struct {
+	Name  *string
+	Email *string
+	Role  *Role
+}
+
+// Role represents a user role
+type Role int32
+
+const (
+	// RoleUnknown represents an unknown user role
+	RoleUnknown Role = 0
+	// RoleUser represents a user role
+	RoleUser Role = 1
+	// RoleAdmin represents an admin role
+	RoleAdmin Role = 2
+)
