8aea5b9f28e4820cbdc6342f291019371a09bf0e
Dmitriy <burindima324@gmail.com>
2024-08-18 19:53:25 +0300

    add getPassword

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,6 +10,7 @@ import (
 type UserRepository interface {
 	Create(ctx context.Context, userCreate *model.UserCreate) (int64, error)
 	Get(ctx context.Context, id int64) (*model.User, error)
+	GetPassword(ctx context.Context, id int64) (string, error)
 	Update(ctx context.Context, id int64, userUpdate *model.UserUpdate) error
 	Delete(ctx context.Context, id int64) error
 	IsExists(ctx context.Context, ids []int64) (bool, error)
